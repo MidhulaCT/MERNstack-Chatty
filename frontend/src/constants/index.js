5f27b6ef6ffcc3dc574e7ca3beee0dce6be4dc49
MidhulaCT <midhulaakshay2020@gmail.com>
2025-07-08 14:43:34 +0530

    Project Completed

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,16 @@
+export const THEMES = [
+    'light',
+    'dark',
+    'cupcake',
+    'bumblebee',
+    'emerald',
+    'corporate',
+    'synthwave',
+    'retro',
+    'cyberpunk',
+    'valentine',
+    'halloween',
+    'garden',
+    'forest',
+    'aqua',
+];
